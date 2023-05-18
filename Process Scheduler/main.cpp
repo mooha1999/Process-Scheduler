56736f6a191fe8d6fd3d2a0da31edb906621f7a6
mooha1999 <50667005+mooha1999@users.noreply.github.com>
2023-05-18 06:24:24 +0300

    main

--- Process Scheduler/main.cpp
+++ Process Scheduler/main.cpp
@@ -1,19 +1,8 @@
 #include <iostream>
+#include "Scheduler.h"
 using namespace std;
-class A {
-public:
-	virtual void fun() {
-		cout << "Hello ";
-	}
-};
-class B : A {
-public:
-	void fun() override {
-		A::fun();
-		cout << "World";
-	}
-};
 int main() {
-	B* b = new B();
-	b->fun();
+	Scheduler scheduler("");
+	scheduler.loadFromFile(""); //Name of the input file
+	scheduler.Simulate();
 }
