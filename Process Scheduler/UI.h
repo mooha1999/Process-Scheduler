81b33ae2417db70a01eafd565484f567a1bbd0eb
mooha1999 <50667005+mooha1999@users.noreply.github.com>
2023-05-18 06:05:27 +0300

    ui

--- Process Scheduler/UI.h
+++ Process Scheduler/UI.h
@@ -10,66 +10,66 @@
 using namespace std;
 class UI
 {
-	void displayProcessorInfo(Processor* processor)
-	{
-		cout << "[" + getProcessorType(processor) + "]: " << processor->GetReadyProcessesCount() << "RDY : ";
-		auto ids = *processor->GetReadyIDs();
-		for (int i = 0; i < ids.Size(); i++) {
-			cout << ids[i] << ", ";
-		}
-	}
 	void displayIDs(Queue<Process*> processes) {
 		while (!processes.IsEmpty())
 		{
 			cout << processes.Pop()->GetId() << ", ";
 		}
-	}
-	string getProcessorType(Processor* processor) {
-		SJF* sjf = dynamic_cast<SJF*>(processor);
-		if (sjf)
-			return "SJF";
-		FCFS* fcfs = dynamic_cast<FCFS*>(processor);
-		if (fcfs)
-			return "FCFS";
-		RR* rr = dynamic_cast<RR*>(processor);
-		if (rr)
-			return "RR";
+		cout << endl;
 	}
 public:
 	void displayStartMessage() {
 		cout << "Simulation started in Interactive mode\n";
 	}
-	void displayInfo(int timestep, LinkedList<FCFS*> fcfsq, LinkedList<SJF*> sjfq, LinkedList<RR*> rrq,
+	void displayInfo(int timestep, LinkedList<FCFS*> fcfsList, LinkedList<SJF*> sjfList, LinkedList<RR*> rrList,
 		Queue<Process*> blockedProcesses, Queue<Process*> terminatedProcesses)
 	{
 		cout << "Current timestep: " << timestep << endl;
 		cout << "--------- RDY processes ---------\n";
 		int runningProcessorsCount = 0;
-		Queue<Processor*> runningProcessors;
-
-		for (int i = 0; i < processors->Size(); i++) {
-			auto temp = processors->operator[](i);
-			cout << "Processor " << i + 1 << " ";
-			displayProcessorInfo(temp);
-			if (temp->GetState() == Processor::BUSY)
-				runningProcessors.Push(temp);
+		int I = 1;
+		for (FCFS* processor : fcfsList) {
+			cout << "Processor " << I++ << "[FCFS]: " << processor->GetReadyProcessesCount() << "RDY: " << processor->GetReadyIDs() << endl;
+			if (processor->GetRunningProcess()) runningProcessorsCount++;
+		}
+		for (SJF* processor : sjfList) {
+			cout << "Processor " << I++ << "[SJF]: " << processor->GetReadyProcessesCount() << "RDY: " << processor->GetReadyIDs() << endl;
+			if (processor->GetRunningProcess()) runningProcessorsCount++;
+		}
+		for (RR* processor : rrList) {
+			cout << "Processor " << I++ << "[RR]: " << processor->GetReadyProcessesCount() << "RDY: " << processor->GetReadyIDs() << endl;
+			if (processor->GetRunningProcess()) runningProcessorsCount++;
 		}
 
 		cout << "--------- BLK processes ---------\n";
-		cout << blockedProcesses->Size() << " BLK: ";
-		displayIDs(*blockedProcesses);
+		cout << blockedProcesses.Size() << " BLK: ";
+		displayIDs(blockedProcesses);
 
 		cout << "--------- RUN processes ---------\n";
-		cout << runningProcessors.Size() << " RUN: ";
-		int I = 1;
-		while (!runningProcessors.IsEmpty()) {
-			cout << runningProcessors.Pop()->GetRunningProcess()->GetId()
-				<< "(P" + to_string(I++) + ")\n";
-		}
+		cout << runningProcessorsCount << " RUN: ";
 
+		I = 1;
+		for (FCFS* processor : fcfsList) {
+			if (processor->GetRunningProcess()) {
+				cout << processor->GetRunningProcess()->GetId() << " (P" << I << "), ";
+			}
+			I++;
+		}
+		for (SJF* processor : sjfList) {
+			if (processor->GetRunningProcess()) {
+				cout << processor->GetRunningProcess()->GetId() << " (P" << I << "), ";
+			}
+			I++;
+		}
+		for (RR* processor : rrList) {
+			if (processor->GetRunningProcess()) {
+				cout << processor->GetRunningProcess()->GetId() << " (P" << I << "), ";
+			}
+			I++;
+		}
 		cout << "--------- TRM processes ---------\n";
-		cout << terminatedProcesses->Size() << " TRM: " << endl;
-		displayIDs(*terminatedProcesses);
+		cout << terminatedProcesses.Size() << " TRM: " << endl;
+		displayIDs(terminatedProcesses);
 		cout << "Enter any key to continue";
 		getchar();
 	}
